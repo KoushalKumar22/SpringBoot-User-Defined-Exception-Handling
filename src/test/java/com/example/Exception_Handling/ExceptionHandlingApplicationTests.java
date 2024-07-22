b79f56905872f92bc6063470af403488e15f0f04
Koushal Kumar <koushalkumar2224@gmail.com>
2024-07-22 23:44:23 +0530

    Deployment

---
+++ src/test/java/com/example/Exception_Handling/ExceptionHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Exception_Handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceptionHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
