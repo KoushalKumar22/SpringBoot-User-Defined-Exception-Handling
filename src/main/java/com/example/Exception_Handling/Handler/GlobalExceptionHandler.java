6aa72df6c660abcb984ba5cbbab45f03407a54a5
Koushal Kumar <koushalkumar2224@gmail.com>
2024-07-23 00:01:52 +0530

    ExceptionHandler

---
+++ src/main/java/com/example/Exception_Handling/Handler/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.example.Exception_Handling.Handler;
+
+import com.example.Exception_Handling.Exceptions.BioNotFoundException;
+import com.example.Exception_Handling.Exceptions.IdNotFoundException;
+import com.example.Exception_Handling.Exceptions.NameNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(IdNotFoundException.class)
+    public ResponseEntity<Object> handleIdNotFoundException(IdNotFoundException exception){
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+
+    @ExceptionHandler(NameNotFoundException.class)
+    public ResponseEntity<Object> handleNameNotFoundException(NameNotFoundException exception){
+        return new ResponseEntity<>(exception.getMessage(),HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BioNotFoundException.class)
+    public ResponseEntity<Object> bioNotFoundException(BioNotFoundException exception){
+        return new ResponseEntity<>(exception.getMessage(),HttpStatus.NOT_FOUND);
+    }
+
+}
